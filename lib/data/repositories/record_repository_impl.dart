f40c57e2f02108ae3f51aa601754c0a4e4cdcc5c
hayanmini <dudals9696@gmail.com>
2025-09-15 13:47:07 +0900

    feat : Data - Repository impl 구현 완료

--- lib/data/repositories/record_repository_impl.dart
+++ lib/data/repositories/record_repository_impl.dart
@@ -1,1 +1,35 @@
-// Domain 인터페이스 구현
+import 'package:flutter_girok_app/data/data_sources/firestore_datasource.dart';
+import 'package:flutter_girok_app/data/models/record_dto.dart';
+import 'package:flutter_girok_app/domain/repositories/record_repository.dart';
+import 'package:flutter_girok_app/domain/models/record_model.dart';
+
+class RecordRepositoryImpl implements RecordRepository {
+  final FirestoreDatasource datasource;
+  RecordRepositoryImpl(this.datasource);
+
+  @override
+  Future<void> addRecord(String userId, RecordModel record) async {
+    final dto = RecordDto.fromDomain(record);
+    await datasource.addRecord(userId, dto);
+  }
+
+  @override
+  Future<void> updateRecord(String userId, RecordModel record) async {
+    final dto = RecordDto.fromDomain(record);
+    await datasource.updateRecord(userId, dto);
+  }
+
+  @override
+  Future<void> deleteRecord(String userId, String recordId) async {
+    await datasource.deleteRecord(userId, recordId);
+  }
+
+  @override
+  Future<List<RecordModel>> getRecordByDate(
+    String userId,
+    DateTime date,
+  ) async {
+    final dtos = await datasource.getRecordByDate(userId, date);
+    return dtos.map((e) => e.toDomain()).toList();
+  }
+}
---
+++ lib/data/repositories/user_repository_impl.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_girok_app/data/data_sources/firestore_datasource.dart';
+import 'package:flutter_girok_app/data/models/user_dto.dart';
+import 'package:flutter_girok_app/domain/models/user.dart';
+import 'package:flutter_girok_app/domain/repositories/user_repository.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final FirestoreDatasource datasource;
+  UserRepositoryImpl(this.datasource);
+
+  @override
+  Future<void> addUser(User user) async {
+    final dto = UserDto.fromDomain(user);
+    await datasource.addUser(dto);
+  }
+
+  @override
+  Future<User?> getUser(String userId) async {
+    final dto = await datasource.getUser(userId);
+    return dto?.toDomain();
+  }
+}

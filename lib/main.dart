c4fcb54bcd172bed2a852c95e21ab23c2232bace
hayanmini <dudals9696@gmail.com>
2025-09-22 20:59:08 +0900

    feat : Main Page 설정

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_girok_app/firebase_options.dart';
 import 'package:flutter_girok_app/presentation/pages/home/main_tab_page.dart';
+import 'package:flutter_girok_app/presentation/pages/login/login_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
@@ -15,6 +16,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: ThemeData.dark(), home: MainTabPage());
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      // home: MainTabPage()
+      home: LoginPage(),
+    );
   }
 }

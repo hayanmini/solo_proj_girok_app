3a0ca9ec1aa5d7b6ecb84b47d3b9b61be1a74118
hayanmini <dudals9696@gmail.com>
2025-09-23 12:59:16 +0900

    feat : 저장 버튼 기능 구현

--- lib/presentation/pages/record/widgets/save_button.dart
+++ lib/presentation/pages/record/widgets/save_button.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_girok_app/core/constants.dart';
 
 class SaveButton extends StatelessWidget {
-  const SaveButton({super.key});
+  final VoidCallback onPressed;
+  const SaveButton({super.key, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     final bottomQuery = MediaQuery.of(context).padding.bottom;
     return GestureDetector(
-      onTap: () {
-        // TODO : Save
-      },
+      onTap: onPressed,
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(

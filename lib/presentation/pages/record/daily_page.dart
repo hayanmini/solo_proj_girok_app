7f1f0062300d403801b234f8f73bffd2d48d2f3a
hayanmini <dudals9696@gmail.com>
2025-09-23 18:19:26 +0900

    feat : Write - Daily UI 및 기능 구현

--- lib/presentation/pages/record/daily_page.dart
+++ lib/presentation/pages/record/daily_page.dart
@@ -1,10 +1,224 @@
+import 'package:firebase_auth/firebase_auth.dart' as fb;
 import 'package:flutter/material.dart';
+import 'package:flutter_girok_app/core/constants.dart';
+import 'package:flutter_girok_app/core/enums.dart';
+import 'package:flutter_girok_app/domain/models/daily.dart';
+import 'package:flutter_girok_app/presentation/pages/record/widgets/save_button.dart';
+import 'package:flutter_girok_app/presentation/providers/record_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class DailyPage extends StatelessWidget {
-  const DailyPage({super.key});
+class DailyPage extends ConsumerStatefulWidget {
+  final Daily? editingRecord;
+  const DailyPage({super.key, this.editingRecord});
+
+  @override
+  ConsumerState<DailyPage> createState() => _DailyPageState();
+}
+
+class _DailyPageState extends ConsumerState<DailyPage> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+  Emotion _selectedEmotion = Emotion.normal;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.editingRecord != null) {
+      _titleController.text = widget.editingRecord!.title;
+      _contentController.text = widget.editingRecord!.content;
+      _selectedEmotion = widget.editingRecord!.emotion;
+    }
+
+    _titleController.addListener(_onChanged);
+    _contentController.addListener(_onChanged);
+  }
+
+  void _onChanged() {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _contentController.dispose();
+    super.dispose();
+  }
+
+  bool get _isSaveEnabled {
+    final titleNotEmpty = _titleController.text.trim().isNotEmpty;
+    final contentNotEmpty = _contentController.text.trim().isNotEmpty;
+    return titleNotEmpty && contentNotEmpty;
+  }
+
+  Future<void> _saveRecord() async {
+    if (!_isSaveEnabled) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("제목이나 할 일을 비워둘 수 없습니다.")));
+      return;
+    }
+
+    final fbUser = fb.FirebaseAuth.instance.currentUser;
+    if (fbUser == null) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("로그인 후 사용 가능합니다.")));
+      return;
+    }
+    try {
+      final userId = fbUser.uid;
+      final now = DateTime.now();
+
+      final record = Daily(
+        id: widget.editingRecord?.id ?? "",
+        title: _titleController.text,
+        createdAt: widget.editingRecord?.createdAt ?? now,
+        updatedAt: now,
+        date: now,
+        emotion: _selectedEmotion,
+        content: _contentController.text,
+      );
+
+      if (widget.editingRecord == null) {
+        await ref.read(recordsProvider.notifier).addRecord(userId, record);
+      } else {
+        await ref.read(recordsProvider.notifier).updateRecord(userId, record);
+      }
+
+      if (context.mounted) Navigator.pop(context);
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("저장 중 오류가 발생했습니다. : $e ")));
+    }
+  }
+
+  IconData _emotionIcon(Emotion emotion) {
+    switch (emotion) {
+      case Emotion.veryBad:
+        return Icons.sentiment_very_dissatisfied;
+      case Emotion.bad:
+        return Icons.sentiment_dissatisfied;
+      case Emotion.normal:
+        return Icons.sentiment_neutral;
+      case Emotion.happy:
+        return Icons.sentiment_satisfied;
+      case Emotion.veryHappy:
+        return Icons.sentiment_very_satisfied;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          // TODO : 캘린더 날짜 변경
+          "9월 20일 토요일",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 8,
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        decoration: BorderBoxDecoration.commonBox,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: Emotion.values.map((emotion) {
+                            final isSelected = _selectedEmotion == emotion;
+                            return GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  _selectedEmotion = emotion;
+                                });
+                              },
+                              child: Container(
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  color: isSelected
+                                      ? Colors.white
+                                      : Colors.grey[800],
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: Icon(
+                                  _emotionIcon(emotion),
+                                  color: isSelected
+                                      ? Colors.orange
+                                      : Colors.white,
+                                  size: 30,
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+
+                      // 제목
+                      Container(
+                        width: double.infinity,
+                        height: 50,
+                        decoration: BorderBoxDecoration.commonBox,
+                        child: TextFormField(
+                          textAlign: TextAlign.center,
+                          cursorColor: Colors.white,
+                          controller: _titleController,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "제목을 입력하세요.",
+                            contentPadding: EdgeInsets.symmetric(horizontal: 8),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+
+                      // 내용
+                      Container(
+                        width: double.infinity,
+                        constraints: BoxConstraints(
+                          minHeight: 200,
+                          maxHeight: 300,
+                        ),
+                        decoration: BorderBoxDecoration.commonBox,
+                        child: TextFormField(
+                          controller: _contentController,
+                          cursorColor: Colors.white,
+                          maxLines: null,
+                          expands: true,
+                          textAlignVertical: TextAlignVertical.top,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "오늘 하루 있었던 일들을 자유롭게 적어보세요.",
+                            contentPadding: EdgeInsets.all(15),
+                          ),
+                          style: TextStyle(fontSize: 16, height: 1.5),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            SaveButton(onPressed: _saveRecord),
+          ],
+        ),
+      ),
+    );
   }
 }

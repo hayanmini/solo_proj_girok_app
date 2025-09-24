f26e92d6a3a9bfa44345f3ce934e90dc7a7c0943
hayanmini <dudals9696@gmail.com>
2025-09-24 15:18:58 +0900

    Write - Series UI 및 기능 구현

--- lib/presentation/pages/record/series_page.dart
+++ lib/presentation/pages/record/series_page.dart
@@ -1,10 +1,379 @@
+import 'package:firebase_auth/firebase_auth.dart' as fb;
 import 'package:flutter/material.dart';
+import 'package:flutter_girok_app/core/constants.dart';
+import 'package:flutter_girok_app/domain/models/folder.dart';
+import 'package:flutter_girok_app/domain/models/series.dart';
+import 'package:flutter_girok_app/presentation/pages/record/widgets/save_button.dart';
+import 'package:flutter_girok_app/presentation/providers/folder_provider.dart';
+import 'package:flutter_girok_app/presentation/providers/record_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SeriesPage extends StatelessWidget {
-  const SeriesPage({super.key});
+class SeriesPage extends ConsumerStatefulWidget {
+  final Series? editingRecord;
+  const SeriesPage({super.key, this.editingRecord});
+
+  @override
+  ConsumerState<SeriesPage> createState() => _SeriesPageState();
+}
+
+class _SeriesPageState extends ConsumerState<SeriesPage> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+  String? _selectedFolderId;
+  String? _selectedFolderName;
+
+  static const String defaultFolderId = "defaultFolderId";
+  static const String defaultFolderName = "기본 폴더";
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.editingRecord != null) {
+      _titleController.text = widget.editingRecord!.title;
+      _contentController.text = widget.editingRecord!.content;
+      _selectedFolderId = widget.editingRecord!.folder;
+      _selectedFolderName = widget.editingRecord!.folder == defaultFolderId
+          ? defaultFolderName
+          : "폴더";
+    } else {
+      _selectedFolderId = defaultFolderId;
+      _selectedFolderName = defaultFolderName;
+    }
+    _titleController.addListener(_onChanged);
+    _contentController.addListener(_onChanged);
+  }
+
+  void _onChanged() {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _contentController.dispose();
+    super.dispose();
+  }
+
+  bool get _isSaveEnabled {
+    return _titleController.text.trim().isNotEmpty &&
+        _contentController.text.trim().isNotEmpty &&
+        _selectedFolderId != null &&
+        _selectedFolderName != null;
+  }
+
+  Future<void> _saveRecord() async {
+    if (!_isSaveEnabled) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("제목과 내용을 입력해주세요.")));
+      return;
+    }
+
+    final fbUser = fb.FirebaseAuth.instance.currentUser;
+    if (fbUser == null) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("로그인 후 사용 가능합니다.")));
+      return;
+    }
+
+    try {
+      final userId = fbUser.uid;
+      final now = DateTime.now();
+      final record = Series(
+        id: widget.editingRecord?.id ?? "",
+        title: _titleController.text,
+        createdAt: widget.editingRecord?.createdAt ?? now,
+        updatedAt: now,
+        date: now,
+        folder: _selectedFolderId!,
+        content: _contentController.text,
+      );
+
+      if (widget.editingRecord == null) {
+        await ref.read(recordsProvider.notifier).addRecord(userId, record);
+      } else {
+        await ref.read(recordsProvider.notifier).updateRecord(userId, record);
+      }
+
+      if (context.mounted) Navigator.pop(context);
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("저장 중 오류가 발생했습니다: $e")));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final foldersAsync = ref.watch(folderAsyncNotifierProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          // TODO : 캘린더 날짜 변경
+          "9월 20일 토요일",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 8,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //
+                    InkWell(
+                      onTap: () => _openFolderSelector(context, foldersAsync),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+
+                        decoration: BorderBoxDecoration.commonBox,
+                        child: Row(
+                          children: [
+                            const Icon(Icons.folder, color: Colors.white),
+                            const SizedBox(width: 8),
+                            Expanded(
+                              child: Text(
+                                _selectedFolderName ?? "기본 폴더",
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+
+                    // 제목
+                    Container(
+                      width: double.infinity,
+                      height: 50,
+                      decoration: BorderBoxDecoration.commonBox,
+                      child: TextFormField(
+                        controller: _titleController,
+                        textAlign: TextAlign.center,
+                        cursorColor: Colors.white,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "제목을 입력하세요.",
+                          contentPadding: EdgeInsets.symmetric(horizontal: 8),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+
+                    // 내용 입력
+                    Container(
+                      width: double.infinity,
+                      height: 300,
+                      decoration: BorderBoxDecoration.commonBox,
+                      child: TextFormField(
+                        controller: _contentController,
+                        cursorColor: Colors.white,
+                        maxLines: null,
+                        expands: true,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "내용을 입력하세요.",
+                          contentPadding: EdgeInsets.all(10),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            SaveButton(onPressed: _saveRecord),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _openFolderSelector(
+    BuildContext context,
+    AsyncValue<List<Folder>> foldersAsync,
+  ) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return foldersAsync.when(
+          data: (folders) {
+            final allFolders = [
+              Folder(
+                id: defaultFolderId,
+                name: defaultFolderName,
+                createdAt: DateTime.now(),
+              ),
+              ...folders.where((f) => f.id != defaultFolderId),
+            ];
+
+            return ListView(
+              children: [
+                ListTile(
+                  leading: const Icon(Icons.create_new_folder_rounded),
+                  title: const Text('새 폴더 만들기'),
+                  onTap: () async {
+                    Navigator.pop(context);
+                    final name = await _showNewFolderDialog(context);
+                    if (name != null && name.trim().isNotEmpty) {
+                      await ref
+                          .read(folderAsyncNotifierProvider.notifier)
+                          .createFolder(name);
+
+                      final updatedFolders =
+                          ref.read(folderAsyncNotifierProvider).value ?? [];
+                      final newFolder = updatedFolders.firstWhere(
+                        (f) => f.name == name,
+                      );
+                      setState(() {
+                        _selectedFolderId = newFolder.id;
+                        _selectedFolderName = newFolder.name;
+                      });
+                    }
+                  },
+                ),
+                const Divider(),
+
+                for (final folder in allFolders)
+                  ListTile(
+                    leading: const Icon(Icons.folder),
+                    title: Text(folder.name),
+                    trailing: folder.id != defaultFolderId
+                        ? Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                icon: const Icon(Icons.edit),
+                                onPressed: () async {
+                                  final controller = TextEditingController(
+                                    text: folder.name,
+                                  );
+                                  final newName = await showDialog<String>(
+                                    context: context,
+                                    builder: (_) => AlertDialog(
+                                      title: const Text("폴더 이름 수정"),
+                                      content: TextField(
+                                        controller: controller,
+                                      ),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () =>
+                                              Navigator.pop(context),
+                                          child: const Text("취소"),
+                                        ),
+                                        TextButton(
+                                          onPressed: () => Navigator.pop(
+                                            context,
+                                            controller.text.trim(),
+                                          ),
+                                          child: const Text("저장"),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                  if (newName != null && newName.isNotEmpty) {
+                                    await ref
+                                        .read(
+                                          folderAsyncNotifierProvider.notifier,
+                                        )
+                                        .updateFolderName(folder.id, newName);
+                                    if (_selectedFolderId == folder.id) {
+                                      setState(() {
+                                        _selectedFolderName = newName;
+                                      });
+                                    }
+                                  }
+                                },
+                              ),
+                              IconButton(
+                                onPressed: () async {
+                                  await _deleteFolder(context);
+                                  // TODO : 삭제 시 바로 반영
+                                },
+                                icon: const Icon(Icons.delete),
+                              ),
+                            ],
+                          )
+                        : null,
+                    onTap: () {
+                      setState(() {
+                        _selectedFolderId = folder.id;
+                        _selectedFolderName = folder.name;
+                      });
+                      Navigator.pop(context);
+                    },
+                  ),
+              ],
+            );
+          },
+          loading: () => const Center(child: CircularProgressIndicator()),
+          error: (e, st) => Center(child: Text("오류: $e")),
+        );
+      },
+    );
+  }
+
+  Future<String?> _showNewFolderDialog(BuildContext context) async {
+    final controller = TextEditingController();
+    return showDialog<String>(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text('새 폴더 이름'),
+        content: TextField(controller: controller),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('취소'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context, controller.text),
+            child: const Text('확인'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _deleteFolder(BuildContext context) async {
+    final confirm = await showDialog<bool>(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text('폴더 삭제'),
+        content: const Text('정말 삭제하시겠습니까?'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context, false),
+            child: const Text('취소'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context, true),
+            child: const Text('삭제'),
+          ),
+        ],
+      ),
+    );
+    if (confirm == true) {
+      await ref
+          .read(folderAsyncNotifierProvider.notifier)
+          .deleteFolder(_selectedFolderId!, "defaultFolderId");
+      setState(() {
+        _selectedFolderId = "defaultFolderId";
+        _selectedFolderName = "기본 폴더";
+      });
+    }
   }
 }

a1d7b46302c3e01b0997b9cd37c83dad549c0411
hayanmini <dudals9696@gmail.com>
2025-09-26 18:14:00 +0900

    refactor : 주석 추가

--- lib/domain/repositories/ai_repository.dart
+++ lib/domain/repositories/ai_repository.dart
@@ -3,5 +3,7 @@ import 'package:flutter_girok_app/domain/models/ai/ai_response.dart';
 
 abstract class AiRepository {
   Future<AiResponse> request(AiRequest req);
+
+  // 스트리밍 모드 사용 시
   Stream<String> streamRequest(AiRequest request);
 }
